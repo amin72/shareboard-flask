8623351c569360b13fe9c2e5a2d3eddfb81592fa
Amin <=>
2022-07-30 20:09:34 +0430

    setup flask app and create home route

---
+++ .flaskenv
@@ -0,0 +1,5 @@
+export FLASK_ENV=development
+export FLASK_APP=src
+export SQLALCHEMY_DB_URI="sqlite:///boards.sqlite"
+export SECRET_KEY="This is the real secret key"
+export JWT_SECRET_KEY="This is jwt secret key"
---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DB_URI'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
+    )
+
+    @app.get('/')
+    def home():
+        return 'Home Page!'
+
+    return app
